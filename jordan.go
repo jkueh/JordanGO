c46600fe1c5e9fc4603146971149d1b92898d574
Jordan Kueh <jordan@zerocube.com.au>
2017-08-27 08:28:01 +1000

    Removing the ping-pong example functionality from the onMessage function - Base functionality is verbose message logging.

--- jordan.go
+++ jordan.go
@@ -117,20 +117,6 @@ func onMessage(s *discordgo.Session, m *discordgo.MessageCreate) {
     m.Author.ID,
     m.Content,
   )
-  // Ignore all messages created by the bot itself
-  // This isn't required in this specific example but it's a good practice.
-  if m.Author.ID == s.State.User.ID {
-    return
-  }
-  // If the message is "ping" reply with "Pong!"
-  if m.Content == "ping" {
-    s.ChannelMessageSend(m.ChannelID, "Pong!")
-  }
-
-  // If the message is "pong" reply with "Ping!"
-  if m.Content == "pong" {
-    s.ChannelMessageSend(m.ChannelID, "Ping!")
-  }
 }
 
 func isDirectMessage(s *discordgo.Session, m *discordgo.MessageCreate) bool {
